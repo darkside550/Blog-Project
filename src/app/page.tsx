c8c2daccff1bc2c41bb1cddbe49ae4e1268f4a4c
Saad <imran.saad50@gmail.com>
2024-08-17 15:20:54 +0530

    Updated content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,10 @@ export default function Home() {
     <main className='text-center pt-32 px-5'>
       <h1 className='text-4xl md:text-5xl font-bold mb-5'>Welcome to my blog</h1>
       <p className='max-w-[750px] mx-auto leading-8'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis accusamus quaerat
-        obcaecati tenetur? Molestiae, quasi consequatur officiis necessitatibus sapiente sit labore
-        laborum. Eius necessitatibus molestiae natus architecto rem. Ullam, modi? Qui debitis fugit
-        eveniet similique in voluptatibus est quasi eaque necessitatibus quo architecto, totam
-        excepturi omnis ipsum quam veritatis nesciunt dolorem dolore provident laborum officia ad.
-        Sint in nam illo. Atque deleniti dolor sequi! Totam excepturi incidunt et mollitia
-        architecto praesentium natus ipsam. Non libero autem fugit velit nam voluptatibus atque
-        numquam illum, soluta rerum, recusandae necessitatibus iusto architecto nemo. Voluptatum,
-        repellendus praesentium ipsam vitae distinctio temporibus consectetur? Rerum cumque officiis
-        temporibus aliquam autem similique nihil natus voluptas unde maiores officia fuga, neque
+        Welcome to my blog app, a project built with Next.js and Prisma ORM and deployed on Vercel.
+        Through this journey, I delved into Next.js routing, explored caching techniques, and
+        successfully integrated Kinde for authentication. This project reflects my hands-on learning
+        experience and showcases the power of modern web technologies.
       </p>
     </main>
   );
